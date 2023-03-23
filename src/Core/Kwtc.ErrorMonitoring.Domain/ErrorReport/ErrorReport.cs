c5fdbb873791ffc4155f7d32fec4f73a719caad9
Kasper Christiansen <kasper@kwc.dk>
2023-03-23 08:43:03 +0100

    Added some repo stuff

--- src/Core/Kwtc.ErrorMonitoring.Domain/ErrorReport/ErrorReport.cs
+++ src/Core/Kwtc.ErrorMonitoring.Domain/ErrorReport/ErrorReport.cs
@@ -4,6 +4,7 @@ using Common;
 
 public class ErrorReport : Auditable
 {
+    public Guid Id { get; set; }
     public string? AppId { get; set; }
     public Severity Severity { get; set; }
     public Exception? OriginalException { get; set; }
--- src/Infrastructure/Kwtc.ErrorMonitoring.Persistence/Reports/ReportRepository.cs
+++ src/Infrastructure/Kwtc.ErrorMonitoring.Persistence/Reports/ReportRepository.cs
@@ -1,16 +1,52 @@
 namespace Kwtc.ErrorMonitoring.Persistence.Reports;
 
+using Application.Abstractions.Database;
+using Dapper;
 using Domain.ErrorReport;
 
 public class ReportRepository : IReportRepository
 {
-    public Task<ErrorReport> AddAsync(ErrorReport errorReport, CancellationToken cancellationToken = default)
+    private readonly IConnectionFactory connectionFactory;
+
+    public ReportRepository(IConnectionFactory connectionFactory)
     {
-        throw new NotImplementedException();
+        this.connectionFactory = connectionFactory;
+    }
+    
+    public async Task<ErrorReport> AddAsync(ErrorReport errorReport, CancellationToken cancellationToken = default)
+    {
+        // TODO: Determine how to persist inner exceptions etc.
+        
+        errorReport.Id = Guid.NewGuid();
+        
+        const string sql = @"
+            INSERT INTO ErrorReports (Id, AppId, Severity, Message, Source, StackTrace, InnerException)
+            VALUES (@Id, @AppId, @Severity, @Message, @Source, @StackTrace, @InnerException)";
+        
+        using var connection = await this.connectionFactory.GetAsync(cancellationToken);
+        await connection.ExecuteAsync(new CommandDefinition(sql, new
+        {
+            errorReport.Id,
+            errorReport.AppId,
+            errorReport.Severity,
+            errorReport.OriginalException?.Message,
+            errorReport.OriginalException?.Source,
+            errorReport.OriginalException?.StackTrace,
+            errorReport.OriginalException?.InnerException
+        }, cancellationToken: cancellationToken));
+
+        return errorReport;
     }
 
-    public Task<IEnumerable<ErrorReport>> GetAllAsync(CancellationToken cancellationToken = default)
+    public async Task<IEnumerable<ErrorReport>> GetAllAsync(CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        // TODO: Probably never going to be used like this.. need to get all by client/user etc.
+        
+        const string sql = @"
+            SELECT Id, AppId, Severity, Message, Source, StackTrace, InnerException
+            FROM ErrorReports";
+
+        using var connection = await this.connectionFactory.GetAsync(cancellationToken);
+        return await connection.QueryAsync<ErrorReport>(new CommandDefinition(sql, cancellationToken: cancellationToken));
     }
 }
