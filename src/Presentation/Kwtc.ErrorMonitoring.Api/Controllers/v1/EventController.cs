825bfdf9585309290d4e3465c9c0626a074f8bfe
Kasper Christiansen <kasper@kwc.dk>
2023-05-28 11:34:16 +0200

    Removing report references and moving to events

--- src/Presentation/Kwtc.ErrorMonitoring.Api/Controllers/v1/EventController.cs
+++ src/Presentation/Kwtc.ErrorMonitoring.Api/Controllers/v1/EventController.cs
@@ -1,5 +1,6 @@
 namespace Kwtc.ErrorMonitoring.Api.Controllers.v1;
 
+using Application.Reports.Commands;
 using Application.Reports.Queries;
 using Attributes;
 using Microsoft.AspNetCore.Mvc;
@@ -17,5 +18,20 @@ public sealed class EventController : ApiControllerBase
         var response = await this.Mediator.Send(new GetReportResponseByClientIdAndAppIdQuery(client.Id, appId), cancellationToken);
 
         return this.Ok(response);
+    }
+
+    [HttpPost]
+    [Route("events")]
+    [Authorization]
+    public async Task<IActionResult> AddEvent(CancellationToken cancellationToken = default)
+    {
+        // TODO: Remove all references to a report and just use an event
+
+        var payload = await this.GetBodyAsStringAsync(cancellationToken);
+        var client = this.GetAuthorizedClient();
+        var report = await this.Mediator.Send(new MapReportPayloadJsonCommand(payload, client.Id), cancellationToken);
+        await this.Mediator.Send(new PersistReportCommand(report), cancellationToken);
+
+        return this.Ok();
     }
 }
